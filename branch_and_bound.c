7ee26e3ff478e864f14836b893280543f437cd77
DisistiNao <gabrielsaldanha373@gmail.com>
2025-03-14 15:10:24 -0300

    Basic Branch and Bound

--- branch_and_bound.c
+++ branch_and_bound.c
@@ -2,29 +2,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void initial_solution(int level, int k, int n, int *result, int *weigh) {
+// Cria uma solução inicial para o problema da Mochila 0-1 sem repetição,
+// onde level é a camada de profundidade na árvore
+// e w é a capacidade máxima da mochila
+void initial_solution(int level, int w, int n, int *result, int *weigh) {
     for(int i = 0; i < n; i++) {
         if(i < level)
-            k -= weigh[i];
-        else if(weigh[i] <= k) {
-            k -= weigh[i];
+            w -= weigh[i];
+        else if(weigh[i] <= w) {
+            w -= weigh[i];
             result[i] = 1;
         }
     }
     return;
 }
 
-int calc_value(int n, int weigh, int value) {
+int calc_value(int n, int result, int value) {
     int value = 0;
 
     for(int i = 0; i < n; i++)
-        value += weigh[i] * value[i];
+        value += result[i] * value[i];
 
     return value;
 }
 
 void bb_knapsack(int w, int *result) {
-    for(int a = 1; a <= 20; a++) {
+    for(int a = 1; a <= ENTRIES; a++) {
         int n = 0;
         char fp[20];  
         sprintf(fp, "Input/%02d.txt", a);
@@ -52,20 +55,23 @@ void bb_knapsack(int w, int *result) {
 
         fclose (file);
         
-        int *initial = (int *)calloc(n, sizeof(int));
-        initial_solution(0, k, n, result, weigh);
-        
-        int better = calc_value(n, weigh, value);
+        initial_solution(0, w, n, result, weigh);
+    
+        int better = calc_value(n, result, value);
 
-        branch_and_bound(n, k, weigh, value, better, result, 1);
+        branch_and_bound(n, w, weigh, value, better, result, 1);
     }
     return;
 }
 
-void branch_and_bound(int n, int k, int *weigh, int *value, int better, int *result, int level) {
-    initial_solution(1, k, n, result, weigh);
-    int local = calc_value(n, weigh, value);
+void branch_and_bound(int n, int w, int *weigh, int *value, int better, int *result, int level) {
+    int *initial = (int *)calloc(n, sizeof(int));
+    initial_solution(1, w, n, initial, weigh);
+    
+    int local = calc_value(n, initial, value);
     
     if(local > better)
-        // dfadsfads
+        
+    else
+        return;
 }
---
+++ branch_and_bound.h
@@ -0,0 +1,19 @@
+#ifndef BRANCH_AND_BOUND_H_
+#define BRANCH_AND_BOUND_H_
+
+// Quantidade de arquivos de entrada
+#define ENTRIES 20
+
+// Cria uma solução inicial para o problema da Mochila 0-1 sem repetição
+void initial_solution(int, int, int, int*, int*);
+
+// 
+int calc_value(int, int, int);
+
+// Resolve o problema da Mochila 0-1 sem repetição com branch-and-bound
+void bb_knapsack(int, int*);
+
+// 
+void branch_and_bound(int, int, int*, int*, int, int*, int);
+
+#endif
